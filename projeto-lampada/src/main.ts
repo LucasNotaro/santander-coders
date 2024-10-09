e90e8f6f87de11b2357200da1f2afc12a975d0e7
LucasNotaro <lucas.sn@live.com>
2024-10-09 13:39:24 -0300

    inclusão do projeto-lampada

--- projeto-lampada/src/main.ts
+++ projeto-lampada/src/main.ts
@@ -1,24 +1,27 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const [botaoLigarDesligar] = document.getElementsByTagName('button');
+const lampada = document.getElementById('lampada') as HTMLImageElement | null;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const cliquesParaQuebrar = Math.floor(Math.random() * 16) + 5; // de 5 a 20
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let contadorDeCliques = 0;
+
+const alternarEstadoLampada = () => {
+    if (lampada!.src.includes('Desligada')) {
+        lampada!.src = '/lampadaLigada.jpg';
+    } else {
+        lampada!.src = '/lampadaDesligada.jpg';
+    }
+};
+
+if (lampada && botaoLigarDesligar) {
+    botaoLigarDesligar.addEventListener('click', () => {
+        contadorDeCliques++;
+
+        if (contadorDeCliques === cliquesParaQuebrar) {
+            lampada.src = '/lampadaQuebrada.jpg';
+            botaoLigarDesligar.disabled = true;
+        } else {
+            alternarEstadoLampada();
+        }
+    });
+}
